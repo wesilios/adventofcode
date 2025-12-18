c80dd9837b097397f01a408d8989119c5b00fb8d
Que Nguyen <quenguyen10190@gmail.com>
2025-12-18 18:50:47 +0700

    feat: implemented logic for day06 part two

--- 2025/netcore/AoC2025/Solutions/Day06/TrashCompactorPartTwo.cs
+++ 2025/netcore/AoC2025/Solutions/Day06/TrashCompactorPartTwo.cs
@@ -26,14 +26,54 @@ public class TrashCompactorPartTwo : ISolution
     public async Task<object> InvokeAsync(bool runTest = false)
     {
         var worksheet = new List<string>();
+        var maxLength = 0;
         await foreach (var line in File.ReadLinesAsync(Path.Combine(Directory.GetCurrentDirectory(),
                            runTest ? Test : Input)))
         {
-            //var rowItems = line.Split(' ').ToList();
             worksheet.Add(line);
+            maxLength = maxLength <= line.Length ? line.Length : maxLength;
         }
 
+        worksheet[^1] += new string(' ', maxLength - worksheet[^1].Length);
         var grandTotal = decimal.Zero;
+
+        var total = new List<decimal>();
+        var operation = "";
+        var x = maxLength - 1;
+        while (x >= 0)
+        {
+            var totalItem = string.Empty;
+            foreach (var row in worksheet)
+            {
+                if (row[x].ToString() == Added || row[x].ToString() == Multiplied)
+                {
+                    operation = row[x].ToString();
+                    break;
+                }
+
+                totalItem += row[x];
+            }
+
+            total.Add(decimal.Parse(totalItem.Trim()));
+            x--;
+
+            if (string.IsNullOrEmpty(operation)) continue;
+
+            switch (operation)
+            {
+                case Multiplied:
+                    grandTotal = total.Aggregate(decimal.One, (current, item) => current * item);
+                    break;
+
+                case Added:
+                    grandTotal += total.Sum();
+                    break;
+            }
+
+            total.Clear();
+            operation = string.Empty;
+            x--;
+        }
 
         return grandTotal;
     }
